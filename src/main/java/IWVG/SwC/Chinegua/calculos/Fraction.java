ea3e6de944292226290c9ff8ee5ecece1a107915
Chinegua <ai.chinegua@gmail.com>
2017-09-16 21:38:05 +0200

    Add Decimal Collection, Fraction, Point, User

---
+++ src/main/java/IWVG/SwC/Chinegua/calculos/Fraction.java
@@ -0,0 +1,29 @@
+package IWVG.SwC.Chinegua.calculos;
+
+public class Fraction {
+    private int numerator;
+
+    private int denominator;
+
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public Fraction() {
+        this(1, 1);
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public double decimal() {
+        return (double) numerator / denominator;
+    }
+
+}
