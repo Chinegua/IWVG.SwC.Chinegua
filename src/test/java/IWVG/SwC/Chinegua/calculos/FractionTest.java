8e4b750ffa0885829320f20e95e3b69967930521
Chinegua <ai.chinegua@gmail.com>
2017-09-17 12:14:08 +0200

    Add test

---
+++ src/test/java/IWVG/SwC/Chinegua/calculos/FractionTest.java
@@ -0,0 +1,36 @@
+package IWVG.SwC.Chinegua.calculos;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FractionTest {
+   private Fraction pt;
+   private Fraction pt2;
+   
+   @Before
+   public void before(){
+       pt2 = new Fraction();
+       pt = new Fraction(1,2);
+    }
+
+    @Test
+    public void testFractionIntInt() {
+        assertEquals(1, pt.getNumerator());
+        assertEquals(2, pt.getDenominator());
+    }
+
+    @Test
+    public void testFraction() {
+        assertEquals(1, pt2.getNumerator());
+        assertEquals(1, pt2.getDenominator());
+    }
+
+
+    @Test
+    public void testDecimal() {
+        assertEquals(0.5, pt.decimal(),0.5);
+    }
+
+}
